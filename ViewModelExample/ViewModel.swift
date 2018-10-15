007d6a140943209a3f33c03a841c81a56469097a
Sergheev Andrian <sergheev@icloud.com>
2018-10-15 20:44:36 +0200

    initial

---
+++ ViewModelExample/ViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModel.swift
+//  ViewModelExample
+//
+//  Created by Andrian Sergheev on 2018-10-15.
+//  Copyright © 2018 Andrian Sergheev. All rights reserved.
+//
+
+import Foundation
+
+
+class ViewModel : NSObject {
+	
+	var sampleNumberOfItems : Int = 10
+	
+	
+}
